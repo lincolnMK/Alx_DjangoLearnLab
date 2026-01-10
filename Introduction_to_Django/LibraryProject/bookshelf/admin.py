e5aab93c55592726b0c4251cae46de562173f4fd
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-10 14:33:22 +0200

    filter list tupple

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -6,5 +6,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('author', 'title',  'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
 
 admin.site.register(Book, BookAdmin)
