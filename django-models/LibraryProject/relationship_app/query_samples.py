3b2a1e73193b831520c165193958c2500be81e91
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-15 16:07:11 +0200

    yes please

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,6 +1,9 @@
-from django.db import models
-from relationship_app import Book, Librarian
 
-books_by_author= Book.objects.filter(author="1984")
-all_books= Book.objects.all()
-librarians = Librarian.objects.all()
+books= Book.objects.filter(author="1984")
+
+books= Book.objects.all()
+print (books.all())
+
+library = Library.objects.get(name=library_name)
+librarian = library.libraries
+print(librarian)
