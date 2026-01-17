b615522e32e1551038e9db8419cde7e8eed1a7c3
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-17 17:44:15 +0200

    kaya mwe

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
-from .views import SignUpView
+from .views import SignUpView , LibraryDetailView, book_list
 
 urlpatterns = [
     path('books/', views.book_list),
     path('about/', views.BookDetailView.as_view()),
+    path('books/', book_list, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     path ('login/', LoginView.as_view(template_name='relationship_app/login.html'), name ='login' ),
     path ('logout/',LogoutView.as_view(template_name='relationship_app/logout'), name = 'logout'),
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,36 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView 
+from django.views.generic import CreateView, DetailView
 
-# Create your views here.
-
-from .models import Book
+from .models import Book, Library
 
 def book_list(request):
-    books = Book.objects.all()  # Fetch all book instances from the database
-    context = {'book_list': books}  # Create a context dictionary with book list
-    return render(request, 'books/list_books.html', context)
+    books = Book.objects.select_related('author')
+    return render(request, 'relationship_app/list_books.html', {
+        'books': books
+    })
 
-from django.views.generic import DetailView , ListView
-from .models import Book
 
 class BookDetailView(DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+    context_object_name = 'book'
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)  # Get default context da
-        book = self.get_object()  # Retrieve the current book instance
-        context['book_list'] = book.objects.all()
+        context = super().get_context_data(**kwargs)
+
+        # Add 'books' attribute dynamically to match the template
+        context['library'].books = context['library'].book.all()
+
         return context
 
 class SignUpView(CreateView):
-	form_class = UserCreationForm
-	success_url = reverse_lazy("login")
-	template_name = "relationship_app/register.html"
-
-class BookListView(ListView):
-	model = Book
-	template_name =  'books/book_list.html'
-
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'relationship_app/register.html'
