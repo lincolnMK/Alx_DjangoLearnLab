9f6d973d82a5df72c0edd4e15078384e7e1064df
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-17 16:41:20 +0200

    kaya man

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,13 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 from . import views
+from .views import SignUpView
 
 urlpatterns = [
     path('books/', views.book_list),
     path('about/', views.BookDetailView.as_view()),
+
+    path ('login/', LoginView.as_view(template_name='relationship_app/login.html'), name ='login' ),
+    path ('logout/',LogoutView.as_view(template_name='relationship_app/logout'), name = 'logout'),
+    path ('register/', SignUpView.as_view(), name="register"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -20,3 +23,9 @@ class BookDetailView(DetailView):
         context = super().get_context_data(**kwargs)  # Get default context da
         book = self.get_object()  # Retrieve the current book instance
         context['average_rating'] = book.get_average_rating() 
+
+
+class SignUpView(CreateView):
+	form_class = UserCreationForm
+	success_url = reverse_lazy("login")
+	template_name = "relationship_app/register.html"
