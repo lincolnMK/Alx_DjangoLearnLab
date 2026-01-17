e52c20e9e7587b383fc4af1edf2b0a91a282c457
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-17 17:06:37 +0200

    impossible

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView 
 
 # Create your views here.
 
@@ -12,7 +12,7 @@ def book_list(request):
     context = {'book_list': books}  # Create a context dictionary with book list
     return render(request, 'books/book_list.html', context)
 
-from django.views.generic import DetailView
+from django.views.generic import DetailView , ListView
 from .models import Book
 
 class BookDetailView(DetailView):
@@ -22,10 +22,15 @@ class BookDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)  # Get default context da
         book = self.get_object()  # Retrieve the current book instance
-        context['average_rating'] = book.get_average_rating() 
-
+        context['book_list'] = book.objects.all()
+        return context
 
 class SignUpView(CreateView):
 	form_class = UserCreationForm
 	success_url = reverse_lazy("login")
 	template_name = "relationship_app/register.html"
+
+class BookListView(ListView):
+	model = Book
+	template_name =  'books/book_list.html'
+
