19f1c74e4b3c8cc9233f45a2a9053697668f63ae
lincolnMK <kingsmkumbwa@gmail.com>
2026-01-17 17:46:38 +0200

    object.all()

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,7 +6,7 @@ from django.views.generic import CreateView, DetailView
 from .models import Book, Library
 
 def book_list(request):
-    books = Book.objects.select_related('author')
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {
         'books': books
     })
