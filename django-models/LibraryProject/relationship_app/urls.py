ccd0ab94ee03885d5dbe2a3332129389d0eefd3c
Kings Lincoln Mkumbwa <kingsmkumbwa@gmail.com>
2026-01-18 09:16:13 +0200

    from omen

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     path ('login/', LoginView.as_view(template_name='relationship_app/login.html'), name ='login' ),
-    path('logout/', views.custom_logout_view, name='logout'),
+    
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
     
     path ('register/', SignUpView.as_view(), name="register"),
