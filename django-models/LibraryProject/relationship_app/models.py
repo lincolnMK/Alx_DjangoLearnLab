12dda6cea83f1b050a22a9998ec2cc7487af39b9
Kings Lincoln Mkumbwa <kingsmkumbwa@gmail.com>
2026-01-18 15:05:56 +0200

    spacers really

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -12,11 +12,13 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='authors')
-    class meta:
+    
+    class Meta:
         permissions = (('can_add_book', "Can add books"),
                        ('can_change_book', "can edit book"),
                        ('can_delete_book', 'can delete book')
-                    )   
+                    )
+        
     def __str__(self):
         return self.title
 
