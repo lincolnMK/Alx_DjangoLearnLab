41433dbd5594a1ae59553ccc38b244d1be696fe9
Kings Lincoln Mkumbwa <kingsmkumbwa@gmail.com>
2026-01-25 09:35:29 +0200

    booklist

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,4 +1,7 @@
+from urllib import request
 from django.shortcuts import render
+
+from LibraryProject.bookshelf.models import Book
 
 # Create your views here.
 
@@ -8,14 +11,27 @@ from django.shortcuts import render
 def edit_book(request, book_id):
     # Logic to edit a book
     pass    
+
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_book(request, book_id):
+def book_list(request):
     # Logic to view a book
-    pass    
+    books = Book.objects.all()
+    return render(request, 'bookshelf/list_books.html', {
+        'books': books
+    })
+
+    
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_detail(request, book_id):
+    # Logic to view book details
+    pass
+
 @permission_required('bookshelf.can_create', raise_exception=True)
 def add_book(request):
     # Logic to add a book
-    pass    
+    pass 
+
 @permission_required('bookshelf.can_delete', raise_exception=True)
 def delete_book(request, book_id):  
     # Logic to delete a book
