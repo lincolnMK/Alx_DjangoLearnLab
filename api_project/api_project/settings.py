859fcc7f5d1f190eb2450ce48a7d890cdf115b63
lincolnMK <kingsmkumbwa@gmail.com>
2026-02-01 12:43:03 +0200

    isnaouthenticated rest drf settings in settings py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -121,5 +121,7 @@ STATIC_URL = 'static/'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.permissions.IsAuthenticated',
+        
     ],
 }   
