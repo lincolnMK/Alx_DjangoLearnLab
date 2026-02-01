6074d723d572e47ba7391608370dff6506bf437e
lincolnMK <kingsmkumbwa@gmail.com>
2026-02-01 10:15:58 +0200

    removed api. when importing serializers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from api.models import Book
-from api.serializers import BookSerializer
+from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
