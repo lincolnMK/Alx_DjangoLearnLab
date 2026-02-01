4338d81b985454ccf4dae6c4311cb4a6ecfe03a2
lincolnMK <kingsmkumbwa@gmail.com>
2026-02-01 12:37:21 +0200

    token aouth imports

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,13 +1,17 @@
-from django.contrib import admin
+
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from api import views
 from api.views import BookList, BookViewSet
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
-router.register(r'books', BookViewSet)
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
     # Include the router URLs for BookViewSet (all CRUD operations)
     path('', include(router.urls)),  # This includes all routes registered with the router
+    path('api/token/', obtain_auth_token, name='api_token_auth'),
+    #path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,54 +1,22 @@
-from django import http
-from rest_framework.decorators import api_view
 from rest_framework.generics import ListAPIView
 from api.models import Book
 from rest_framework import viewsets
 from .serializers import BookSerializer
-from rest_framework.response import Response
-
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
 
 # Create your views here.
 class BookList(ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+   
     
 class BookViewSet(viewsets.ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+
 
     
-@api_view(['GET', 'POST'])   
-def Book_List(request):
-    if request.method == 'GET':
-        books = Book.objects.all()
-        serializer = BookSerializer(books, many=True)
-        return Response(serializer.data)
-    
-    if request.method =='POST':
-        serializer = BookSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=http.HTTP_201_CREATED)
-        return Response(serializer.errors, status=http.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def Book_Detail(request, pk):
-    try:
-        book = Book.objects.get(pk=pk)
-    except Book.DoesNotExist:
-        return Response(status=http.HTTP_404_NOT_FOUND)
-    
-    if request.method == 'GET':
-        serializer = BookSerializer(book)
-        return Response(serializer.data)
-    
-    if request.method == 'PUT':
-        serializer = BookSerializer(book, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=http.HTTP_400_BAD_REQUEST)
-    
-    if request.method == 'DELETE':
-        book.delete()
-        return Response(status=http.HTTP_204_NO_CONTENT)
