7d2eb7a75576d5dd859bd51dfd08630be07aa1b3
kings mkumbwa <kingsmkumbwa@gmail.com>
2026-02-14 20:03:42 +0200

    sluggish

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -54,7 +54,7 @@ urlpatterns = [
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
-    path('tag/<slug:tag_slug>/', PostByTagListView.as_view(), name='tag-posts'),
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='tag-posts'),
     path("search/", PostSearchView.as_view(), name="post-search"),
 
 
